8b1a75517677198212ec9344cae892361e9d4932
Ali Buzayan <abuzayan@ucsd.edu>
2021-01-28 10:57:38 -0800

    finished through slide 41

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,33 @@
  */
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index', {
+  	"projects": [
+  	{ 
+  		"name": "Waiting in line",
+  	  	"image": "lorempixel.people.1.jpeg",
+  	  	"id": "project1"
+  	},  
+  	{ 
+  		"name": "Needfinding",
+  	  	"image": "lorempixel.city.1.jpeg",
+  	  	"id": "project2"
+  	},
+  	{
+  		"name": "Ford GoBike Analysis",
+  		"image": "lorempixel.people.2.jpeg",
+  		"id": "project3"
+  	},
+  	{
+  		"name": "2048 Clone",
+  		"image": "lorempixel.technics.1.jpeg",
+  		"id": "project4"
+  	},
+  	{
+  		"name": "Nvidia Jetson Robot",
+  		"image": "lorempixel.abstract.1.jpeg",
+  		"id": "project5"
+  	}
+  	]
+  });
 };
